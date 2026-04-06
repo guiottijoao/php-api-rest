c766f1d86fd3fa2be73704da7de4ef485ae2fb7c
guiottijoao <joaovitorguiotti8@gmail.com>
2026-04-06 16:38:12 -0300

    FEAT: Add cors control at index.php

--- back/src/index.php
+++ back/src/index.php
@@ -8,6 +8,15 @@ require_once 'controllers/OrderItemController.php';
 require_once 'controllers/FinishOrderController.php';
 require_once 'controllers/CancelOrderController.php';
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(200);
+  exit;
+}
+
 $database =  new Database();
 $db = $database::getConnection();
 
