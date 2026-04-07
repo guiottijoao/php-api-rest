52d2c139aed51e9d9a0151c297d58c9af1ebfa5e
guiottijoao <joaovitorguiotti8@gmail.com>
2026-04-07 17:53:50 -0300

    FIX: Update order item reducer name

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -7,6 +7,6 @@ export const store = configureStore({
   reducer: {
     categories: categoryReducer,
     products: productReducer,
-    orderItem: orderItemReducer
+    orderItems: orderItemReducer
   }
 })
--- front/src/store/slices/orderItemSlice.js
+++ front/src/store/slices/orderItemSlice.js
@@ -36,7 +36,7 @@ export const deleteOrderItem = createAsyncThunk(
 );
 
 const orderItemSlice = createSlice({
-  name: "orderItem",
+  name: "orderItems",
 
   initialState: {
     items: [],
